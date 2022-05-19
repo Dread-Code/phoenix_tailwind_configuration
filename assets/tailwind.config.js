4fe50310f52948ca8daa6f678a0d34106535290f
DreadfulCrazy <lucasospina97@gmail.com>
2022-05-19 00:10:16 -0500

    setup tailwind and postcss

---
+++ assets/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [],
+  mode: "jit",
+  purge: ["../lib/*_web/**/*.ëx", "./js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ import Config
 
 # Configure your database
 config :petal, Petal.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "dreadfulcrazy",
+  password: "",
   hostname: "localhost",
   database: "petal_dev",
   stacktrace: true,
@@ -25,6 +25,14 @@ config :petal, PetalWeb.Endpoint,
   debug_errors: true,
   secret_key_base: "BZTll/Np/9C1dZZdbPY2QpvL9dTHnnZggsCWFGu8PZ0A7Gt0ya5HcBYVGc0RNa1z",
   watchers: [
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ],
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
   ]
